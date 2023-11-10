5215f456c44cabc1d056703bfbc10d22ffffbee6
md149 <md149@hdm-stuttgart.de>
2023-11-10 11:58:05 +0100

    Added: player rotation towards mouse

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,32 +6,56 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
 
-    [SerializeField] private InputAction wasd;
-    [SerializeField] private CharacterController player;
+    [SerializeField] private InputAction _movePlayer;
+    [SerializeField] private CharacterController _playerController;
+    [SerializeField] private Camera _mainCamera;
 
-
-    private void OnEnable()
-    {
-        wasd.Enable();
-    }
-    private void OnDisable()
-    {
-        wasd.Disable();
-    }
-
-    void Start()
-    {
-
-    }
-
+    [Range(20f, 80f)]
+    [SerializeField] 
+    private float _moveSpeed;
 
     void Update()
     {
-        // for 3D movement
-        player.transform.position += wasd.ReadValue<Vector3>() * 20 * Time.deltaTime;
-
-        Debug.Log(wasd.ReadValue<Vector3>().ToString());
+        MovePlayer();
+        RotatePlayerTowardsMouse();
+    }
+    private void OnEnable()
+    {
+        _movePlayer.Enable();
+    }
+    private void OnDisable()
+    {
+        _movePlayer.Disable();
     }
 
+    void MovePlayer()
+    {
+        // Move player according to input value
+        _playerController.Move(_movePlayer.ReadValue<Vector3>() * _moveSpeed * Time.deltaTime);
+    }
 
+    void RotatePlayerTowardsMouse()
+    {
+        Vector3 mousePosition = Mouse.current.position.ReadValue();
+
+        // Convert mouse position to a point in the world space
+        Ray ray = _mainCamera.ScreenPointToRay(mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            Vector3 targetPosition = hit.point;
+
+            // Calculate the direction to the target position
+            Vector3 direction = targetPosition - _playerController.transform.position;
+            direction.y = 0f;
+
+            // Rotate the player to face the mouse position
+            if (direction.magnitude > 0.1f)
+            {
+                Quaternion toRotation = Quaternion.LookRotation(direction, Vector3.up);
+                _playerController.transform.rotation = Quaternion.RotateTowards(
+                    _playerController.transform.rotation, toRotation, 720 * Time.deltaTime);
+            }
+        }
+    }
 }
